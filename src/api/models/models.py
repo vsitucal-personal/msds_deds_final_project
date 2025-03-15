51accb77df2bce0a2d9095ca2eb7c0853010fcc3
Vincent Errick S Itucal <vsitucal@gmail.com>
2025-03-16 06:52:07 +0800

    Update models.py

--- src/api/models/models.py
+++ src/api/models/models.py
@@ -2,7 +2,6 @@ from pydantic import BaseModel
 
 
 class Customer(BaseModel):
-    customer_id: int
     first_name: str
     last_name: str
     email: str
@@ -10,14 +9,12 @@ class Customer(BaseModel):
 
 
 class Vendor(BaseModel):
-    vendor_id: int
     vendor_name: str
     region: str
     joined_at: str
 
 
 class InventoryItem(BaseModel):
-    item_id: int
     vendor_id: int
     item_name: str
     category: str
