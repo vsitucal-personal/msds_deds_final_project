b7199d215e6b52bd807ba9a9b0a08abac91328fe
Vincent Errick S Itucal <vsitucal@gmail.com>
2025-03-16 06:46:35 +0800

    progress

--- src/api/api.py
+++ src/api/api.py
@@ -1,8 +1,9 @@
 import os
 
-import psycopg2
-from fastapi import FastAPI, Query
-from models.models import Customer, InventoryItem, Vendor
+import psycopg2.extras
+from fastapi import FastAPI, HTTPException
+from models.models import Customer
+from psycopg2 import IntegrityError
 from psycopg2.extras import RealDictCursor
 
 POSTGRES_USER = os.getenv("POSTGRES_USER")
@@ -22,75 +23,30 @@ def get_db_connection():
     return conn
 
 
-@app.post("/customers/")
+@app.post("/customers/", status_code=201)
 def create_customer(customer: Customer):
     conn = get_db_connection()
     cursor = conn.cursor()
-    cursor.execute(
-        "INSERT INTO customer (first_name, last_name, email, joined_at)"
-        " VALUES (%s, %s, %s, %s) RETURNING *",
-        (customer.first_name, customer.last_name, customer.email, customer.joined_at),
-    )
-    new_customer = cursor.fetchone()
-    conn.commit()
-    cursor.close()
-    conn.close()
-    return new_customer
-
-
-@app.post("/vendors/")
-def create_vendor(vendor: Vendor):
-    conn = get_db_connection()
-    cursor = conn.cursor()
-    cursor.execute(
-        "INSERT INTO vendor (vendor_name, region, joined_at) VALUES (%s, %s, %s) RETURNING *",
-        (vendor.vendor_name, vendor.region, vendor.joined_at),
-    )
-    new_vendor = cursor.fetchone()
-    conn.commit()
-    cursor.close()
-    conn.close()
-    return new_vendor
-
-
-@app.post("/inventory/")
-def create_inventory(item: InventoryItem):
-    conn = get_db_connection()
-    cursor = conn.cursor()
-    cursor.execute(
-        "INSERT INTO inventory (item_name, category, price, updated_at, vendor_id) "
-        "VALUES (%s, %s, %s, %s, %s) RETURNING *",
-        (item.item_name, item.category, item.price, item.updated_at, item.vendor_id),
-    )
-    new_item = cursor.fetchone()
-    conn.commit()
-    cursor.close()
-    conn.close()
-    return new_item
-
-
-@app.get("/inventory/")
-def get_inventory(item_name: str = Query(None)):
-    conn = get_db_connection()
-    cursor = conn.cursor()
-    if item_name:
+    try:
         cursor.execute(
-            "SELECT * FROM inventory WHERE item_name ILIKE %s", (f"%{item_name}%",)
+            """
+            INSERT INTO customer (first_name, last_name, email, joined_at)
+            VALUES (%s, %s, %s, %s) RETURNING *;
+            """,
+            (
+                customer.first_name,
+                customer.last_name,
+                customer.email,
+                customer.joined_at,
+            ),
         )
-    else:
-        cursor.execute("SELECT * FROM inventory")
-    inventory = cursor.fetchall()
-    cursor.close()
-    conn.close()
-    return inventory
+        new_customer = cursor.fetchone()
+        conn.commit()
+    except IntegrityError:
+        conn.rollback()
+        raise HTTPException(status_code=400, detail="Email already exists")
+    finally:
+        cursor.close()
+        conn.close()
 
-
-@app.get("/vendors/{vendor_id}/inventory")
-def get_vendor_inventory(vendor_id: int):
-    conn = get_db_connection()
-    cursor = conn.cursor()
-    cursor.execute("SELECT * FROM inventory WHERE vendor_id = %s", (vendor_id,))
-    vendor_inventory = cursor.fetchall()
-    cursor.close()
-    conn.close()
-    return vendor_inventory
+    return new_customer
